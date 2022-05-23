333d5384a1200de1dd9185856e968c9b82c169dd
ShadaSabea <103149059+ShadaSabea@users.noreply.github.com>
2022-05-23 22:22:02 +0300

    Update HealthPoints.h

--- HealthPoints.h
+++ HealthPoints.h
@@ -1,112 +1,64 @@
-
 //
 // Created by Shada Sabea on 22/05/2022.
 //
 
-#include "HealthPoints.h"
-
-HealthPoints::HealthPoints(const int healthPoints): m_currentHealthPoints(healthPoints), m_maxHealthPoints(healthPoints)
-{
-    if(healthPoints<=0)
-    {
-        throw InvalidArgument();
-    }
-}
-HealthPoints HealthPoints::operator+(const int num) const
-{
-    HealthPoints  healthPoints1=m_currentHealthPoints+num;
-    if(healthPoints1.m_currentHealthPoints>m_maxHealthPoints)
-    {
-        healthPoints1.m_currentHealthPoints=m_maxHealthPoints;
-    }
-    if (healthPoints1.m_currentHealthPoints<0)
-    {
-        healthPoints1.m_currentHealthPoints=0;
-    }
-
-    return healthPoints1;
-
-}
-
-HealthPoints HealthPoints::operator-(const int num) const
-{
-HealthPoints  healthPoints1=m_currentHealthPoints-num;
-if(healthPoints1.m_currentHealthPoints<0)
-{
-    healthPoints1.m_currentHealthPoints=0;
-}
-if(healthPoints1.m_currentHealthPoints>m_maxHealthPoints)
-{
-    healthPoints1.m_currentHealthPoints=m_maxHealthPoints;
-}
-return healthPoints1;
-}
-
-HealthPoints& HealthPoints::operator+=(const int num)
-{
-    m_currentHealthPoints+=num;
-    return *this;
-}
-
-HealthPoints& HealthPoints::operator-=(const int num)
-{
-    m_currentHealthPoints-=num;
-    return *this;
-}
-
- bool operator==(const HealthPoints& healthPoints1, const HealthPoints& healthPoints2)
-{
-    if(healthPoints1.m_currentHealthPoints==healthPoints2.m_currentHealthPoints)
-    {
-        return true;
-    }
-    return false;
-}
-
-bool operator<(const HealthPoints& healthPoints1,const HealthPoints& healthPoints2)
-{
-    if(healthPoints1.m_currentHealthPoints<healthPoints2.m_currentHealthPoints)
-    {
-        return true;
-    }
-    return false;
-}
+#ifndef EX3_HEALTHPOINTS_H
+#define EX3_HEALTHPOINTS_H
+#include <ostream>
 
 
- std::ostream& operator<<(std::ostream& os , const HealthPoints& healthPoints)
-{
-    os<<healthPoints.m_currentHealthPoints<<"("<<healthPoints.m_maxHealthPoints<<")";
-}
 
-bool operator<=(const HealthPoints& healthPoints1,const HealthPoints& healthPoints2)
-{
-    if (healthPoints1 == healthPoints2 || healthPoints1 < healthPoints2) {
-        return true;
-    }
-    return false;
-}
+class HealthPoints{
+public:
+    /**
+     * constructor
+     * @param healthPoints the number of health points
+     */
+HealthPoints(const int m_currentHealthPoints=100);
 
-bool operator!=(const HealthPoints& healthPoints1,const HealthPoints& healthPoints2)
-{
-    return (!(healthPoints1==healthPoints2));
-}
+HealthPoints(const HealthPoints& healthPoints)=default;
 
-bool operator>=(const HealthPoints& healthPoints1,const HealthPoints& healthPoints2)
-{
-    if(!(healthPoints1<healthPoints2))
-    {
-        return true;
-    }
-    return false;
-}
+~HealthPoints()=default;
 
-bool operator>(const HealthPoints& healthPoints1,const HealthPoints& healthPoints2)
-{
-    if(!(healthPoints1<=healthPoints2))
-    {
-        return true;
-    }
-    return false;
-}
+HealthPoints& operator=(const HealthPoints& healthPoints)=default;
 
 
+/**
+ *
+ * @param num the number that will be added to the current health points
+ * @return
+ */
+HealthPoints operator+(const int num) const;
+/**
+ *
+ * @param num the number that will be deducted to the current health points
+ * @return
+ */
+HealthPoints operator-(const int num) const;
+
+HealthPoints& operator+=(const int num);
+
+HealthPoints& operator-=(const int num);
+
+ class InvalidArgument{};
+
+
+private:
+   int m_currentHealthPoints;
+   int m_maxHealthPoints;
+    friend bool operator==(const HealthPoints& healthPoints1, const HealthPoints& healthPoints2);
+    friend std::ostream& operator<<(std::ostream&, const HealthPoints&);
+    friend bool operator<(const HealthPoints& healthPoints1,const HealthPoints& healthPoints2);
+};
+
+
+
+
+
+bool operator<=(const HealthPoints& healthPoints1,const HealthPoints& healthPoints2) ;
+
+bool operator>=(const HealthPoints& healthPoints1,const HealthPoints& healthPoints2) ;
+
+bool operator>(const HealthPoints& healthPoints1,const HealthPoints& healthPoints2) ;
+
+#endif //EX3_HEALTHPOINTS_H

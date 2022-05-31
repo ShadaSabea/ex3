b01d149c4a6ec61ca91afeb7ec818f90f30f793c
ShadaSabea <103149059+ShadaSabea@users.noreply.github.com>
2022-06-01 00:03:33 +0300

    Update HealthPoints.h

--- HealthPoints.h
+++ HealthPoints.h
@@ -14,56 +14,123 @@ public:
      * constructor
      * @param healthPoints the number of health points
      */
-    HealthPoints(const int m_currentHealthPoints=100);
-
-    HealthPoints(const HealthPoints& healthPoints)=default;
-
-    ~HealthPoints()=default;
-
-    HealthPoints& operator=(const HealthPoints& healthPoints)=default;
-
-    HealthPoints& operator=(const int healthPoints);
+HealthPoints(int m_currentHealthPoints=100);
 
 
-    HealthPoints& operator+=(const HealthPoints& healthPoints);
+/**
+ * copy constructor
+ * @param healthPoints
+ */
+HealthPoints(const HealthPoints& healthPoints)=default;
 
-    HealthPoints& operator-=(const HealthPoints& healthPoints);
-
-    /**
- *
- * @param num the number that will be deducted to the current health points
- * @return
+/**
+ * destructor
+ */
+~HealthPoints()=default;
+/**
+ * operator = between 2 objects
+ * @param healthPoints
+ * @return object after being updated
+ */
+HealthPoints& operator=(const HealthPoints& healthPoints)=default;
+/**
+ * operator = between object and int
+ * @param healthPoints
+ * @return  object after being updated
+ */
+HealthPoints& operator=(int healthPoints);
+/**
+ * operator +=
+ * @param healthPoints
+ * @return  adding the received  health points to my health points
  */
 
+HealthPoints& operator+=(const int increase);
+
+/**
+ * operator -=
+ * @param healthPoints
+ * @return removing health points the same as being accepted
+ */
+HealthPoints& operator-=(const int decrease);
 
 
-    class InvalidArgument{};
+ class InvalidArgument{};
 
 
 private:
-    int m_currentHealthPoints;
-    int m_maxHealthPoints;
+   int m_currentHealthPoints;
+   int m_maxHealthPoints;
+   /**
+    *operator ==
+    * @param healthPoints1
+    * @param healthPoints2
+    * @return true if the two objects are the same
+    */
     friend bool operator==(const HealthPoints& healthPoints1, const HealthPoints& healthPoints2);
-    friend std::ostream& operator<<(std::ostream&, const HealthPoints&);
+    /**
+     *
+     * @return printing data
+     */
+    friend std::ostream& operator<<(std::ostream& os, const HealthPoints& healthPoints);
+    /**
+     * comparison between two objects
+     * @param healthPoints1
+     * @param healthPoints2
+     * @return true if the objects are te same
+     */
     friend bool operator<(const HealthPoints& healthPoints1,const HealthPoints& healthPoints2);
 };
 
 
 
 
-
+/**
+ * removing health points object from another
+ * @param healthPoints1
+ * @param num
+ * @return object after being changed
+ */
 HealthPoints operator-(const HealthPoints& healthPoints1,const int num);
 
+HealthPoints operator-(const int num,const HealthPoints& healthPoints1);
 
+/**
+ *
+ * @param num
+ * @param healthPoints2
+ * @return sum of the 2 health points
+ */
 HealthPoints operator+(const int num,const HealthPoints& healthPoints2);//000000000
+/**
+ *
+ * @param healthPoints2
+ * @param num
+ * @return sum of the 2 health points
+ */
 HealthPoints operator+(const HealthPoints& healthPoints2,const int num);//000000000
 
-
+/**
+ * comparison between 2 objects
+ * @param healthPoints1
+ * @param healthPoints2
+ * @return true if the first object <= second object else false
+ */
 bool operator<=(const HealthPoints& healthPoints1,const HealthPoints& healthPoints2) ;
-
+/**
+ * comparison between 2 objects
+ * @param healthPoints1
+ * @param healthPoints2
+ * @return true if the first object >= second object
+ */
 bool operator>=(const HealthPoints& healthPoints1,const HealthPoints& healthPoints2) ;
-
-bool operator>(const HealthPoints& healthPoints1,const HealthPoints& healthPoints2) ;
+/**
+ * comparison between 2 objects
+ * @param healthPoints1
+ * @param healthPoints2
+ * @return true if the first object > second object
+ */
+bool operator>(const HealthPoints& healthPoints1,const HealthPoints& healthPoints2);
 
 bool operator!=(const HealthPoints& healthPoints1,const HealthPoints& healthPoints2);
 
